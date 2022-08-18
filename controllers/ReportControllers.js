1909e826b6d636ae0d0cec04163e7c78d147eefe
h2o-Noob <arindamraina7743@gmail.com>
2022-08-18 10:07:37 +0530

    36

--- controllers/ReportControllers.js
+++ controllers/ReportControllers.js
@@ -1,49 +1,78 @@
 const reportSchema = require("../models/ReportModel");
 const ApiFeatures = require("../utils/apiFeatures");
 const ErrorHandler = require("../utils/errorHandler");
+const cloudinary = require("cloudinary");
 
 // create report
 exports.createReport = async (req, res, next) => {
-    try {
-  
-      const number = await reportSchema.countDocuments() + 1
-  
-      req.body.number = number
-      req.body.user = req.User.id;
+  try {
 
-      const report = await reportSchema.create(req.body);
-      
-      res.status(200).json({
-        message: "report created",
-        report,
-        success: true
-      });
-    } catch (error) {
-      next(new ErrorHandler(error))
+    let images = [];
+
+    if (typeof req.body.images === "string") {
+      images.push(req.body.images);
     }
+
+    if (images.length==0) {
+      images.push(
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg"
+      );
+    }
+
+    const imagesLinks = [];
+
+    for (let i = 0; i < images.length; i++) {
+      const result = await cloudinary.uploader.upload(images[i], {
+        folder: "reports",
+      });
+
+      imagesLinks.push({
+        public_id: result.public_id,
+        url: result.secure_url,
+      });
+    }
+
+    req.body.images = imagesLinks;
+
+    const number = (await reportSchema.countDocuments()) + 1;
+
+    req.body.number = number;
+    req.body.user = req.User.id;
+
+    const report = await reportSchema.create(req.body);
+
+    res.status(200).json({
+      message: "report created",
+      report,
+      success: true,
+    });
+  } catch (error) {
+    next(new ErrorHandler(error));
+  }
 };
 
 // get all reports
 exports.getAllReports = async (req, res, next) => {
   try {
-    
-    const resultPerPage = 8
+    const resultPerPage = 8;
 
-    const apiFeatures = new ApiFeatures(reportSchema.find().populate("user", "name avatar email"), req.query)
+    const apiFeatures = new ApiFeatures(
+      reportSchema.find().populate("user", "name avatar email"),
+      req.query
+    )
       .search()
       .pagination(resultPerPage);
 
-      const reports = await apiFeatures.query;
+    const reports = await apiFeatures.query;
 
     res.status(200).json({
       success: true,
       reports,
     });
-
   } catch (error) {
-    next(new ErrorHandler(error))
+    next(new ErrorHandler(error));
   }
-}
+};
 
 // user reports
 exports.myReports = async (req, res, next) => {
@@ -62,8 +91,9 @@ exports.myReports = async (req, res, next) => {
 // get report details
 exports.getReportDetails = async (req, res, next) => {
   try {
-    
-    const report = await reportSchema.findById(req.params.id).populate("user", "name avatar email");
+    const report = await reportSchema
+      .findById(req.params.id)
+      .populate("user", "name avatar email");
 
     if (!report) {
       return next(new ErrorHandler("report not found", 404));
@@ -74,15 +104,13 @@ exports.getReportDetails = async (req, res, next) => {
       report,
     });
   } catch (error) {
-    next(new ErrorHandler(error))
+    next(new ErrorHandler(error));
   }
-}
+};
 
 // update report
 exports.updateReport = async (req, res, next) => {
-
   try {
-    
     report = await reportSchema.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true,
@@ -92,18 +120,16 @@ exports.updateReport = async (req, res, next) => {
     res.status(200).json({
       message: "report updated",
       report,
-      success: true
+      success: true,
     });
   } catch (error) {
-    next(new ErrorHandler(error))
+    next(new ErrorHandler(error));
   }
-}
+};
 
 // delete report
 exports.deleteReport = async (req, res, next) => {
-
   try {
-    
     const report = await reportSchema.findByIdAndDelete(req.params.id);
 
     if (!report) {
@@ -115,6 +141,6 @@ exports.deleteReport = async (req, res, next) => {
       message: "report deleted",
     });
   } catch (error) {
-    next(new ErrorHandler(error))
+    next(new ErrorHandler(error));
   }
-}
+};
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
+var cloudinary = require('cloudinary');
 
 dotenv.config({ path: "config/config.env" });
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+})
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`server listening at http://localhost:${process.env.PORT}`);
